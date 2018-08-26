3ff67e355721e141c6a2d487b5a6e1b2ca0cfdf4
Ervandra Halim <ervandra.halim@gmail.com>
2018-08-26 16:46:13 +0700

    First commit, add cmobet, joglototo, ovo88

---
+++ joglototo/js/app-mobile.js
@@ -0,0 +1,15 @@
+jQvan = jQuery.noConflict();
+jQvan(document).ready(function($){
+  $(document).foundation();
+});
+// function addMeta(){
+//   var meta=document.createElement('meta');
+//   meta.name='viewport';
+
+//   meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+
+//   document.getElementsByTagName('head')[0].appendChild(meta);
+// }
+// function removeMeta(){
+//   document.getElementsByName('viewport')[0].remove();
+// }
---
+++ ovo88/js/home.js
@@ -0,0 +1,22 @@
+jQhome = jQuery.noConflict();
+jQhome(document).ready(function($){
+  $('.main-slideshow').slick({
+    prevArrow: '<button class="slick-nav slick-prev"><span class="fa fa-angle-left"></span></button>',
+    nextArrow: '<button class="slick-nav slick-next"><span class="fa fa-angle-right"></span></button>'
+  });
+  $('.running-banner').slick({
+    autoplay:true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    pauseOnHover: true,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    buttons:false,
+    variableWidth: true,
+    infinite: true,
+    arrows: false
+  });
+});
