241c021e9d2068bf3c446c308345a8291dde9631
Ervandra Halim <ervandra.halim@gmail.com>
2018-08-29 16:08:28 +0700

    update indobookies home

--- indobookies/js/app.js
+++ indobookies/js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+jQvan = jQuery.noConflict();
+jQvan(document).ready(function($){
+  $(document).foundation();
+  $('#home-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+})
