331dafed06b67b5987e5db16f36b50764b2dafe9
Ervandra Halim <ervandra.halim@gmail.com>
2018-09-13 13:31:56 +0700

    add footer info, and autoplay main slideshow

--- ovo88/js/home.js
+++ ovo88/js/home.js
@@ -1,6 +1,7 @@
 jQhome = jQuery.noConflict();
 jQhome(document).ready(function($){
   $('.main-slideshow').slick({
+    autoplay: true,
     prevArrow: '<button class="slick-nav slick-prev"><span class="fa fa-angle-left"></span></button>',
     nextArrow: '<button class="slick-nav slick-next"><span class="fa fa-angle-right"></span></button>'
   });
