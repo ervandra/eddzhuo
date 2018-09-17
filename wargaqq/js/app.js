9a6054f1b617182a50c52e134398b64aa2953f8f
Ervandra Halim <ervandra.halim@gmail.com>
2018-09-17 22:01:28 +0700

    change background and several theme color

--- wargaqq/js/app.js
+++ wargaqq/js/app.js
@@ -5,4 +5,18 @@ jQvan(document).ready(function($){
     fade: true,
     autoplay: true
   });
+
+  var count = 111069595;
+  var count2 = 312069531;
+  var no = count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  var no2 = count2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  setInterval(function(){
+    count = count + Math.floor(Math.random() * 10 + 3);
+    count2 = count2 + Math.floor(Math.random() * 10 + 5);
+    no = count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    no2 = count2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    $('#jackpot-poker').text('$'+no);
+    $('#jackpot-domino').text('$'+no2);
+  }, Math.floor((Math.random() * 100) + 100 ))
+
 })
