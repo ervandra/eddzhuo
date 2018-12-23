8a56eebd7e51be3b117d1de9a7e981739105ec2f
Ervandra Halim <ervandra.halim@gmail.com>
2018-12-23 17:11:59 +0700

    remove toggle footer contact

--- astonwin/js/app.js
+++ astonwin/js/app.js
@@ -3,13 +3,13 @@ jQvan(document).ready(function($){
   $(document).foundation();
 
   // hide livechat
-  $(document).click(function(event) {
-    if(!$(event.target).closest('#footer-contact').length) {
-        if($('#footer-contact').hasClass('open')) {
-            $('#footer-contact').removeClass('open');
-        }
-    }
-  });
+  // $(document).click(function(event) {
+  //   if(!$(event.target).closest('#footer-contact').length) {
+  //       if($('#footer-contact').hasClass('open')) {
+  //           $('#footer-contact').removeClass('open');
+  //       }
+  //   }
+  // });
 
   $('.main-slideshow').slick({
     autoplay: true,
