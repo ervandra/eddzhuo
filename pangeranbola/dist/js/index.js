a8f46dd3f38f401e6fd0e6275a2f71784d8abe0f
Ervandra Halim <ervandra.halim@gmail.com>
2019-06-07 14:24:25 +0700

    add pangeranbola

---
+++ pangeranbola/dist/js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var jQvan = jQuery.noConflict();
+jQvan(document).ready(function ($) {
+  $('body').addClass('ready');
+  $(document).foundation();
+  $('#slider').nivoSlider({});
+});
